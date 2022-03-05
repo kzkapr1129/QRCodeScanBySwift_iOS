3b2b5985bbde75bedece374bb17bb1fe106125ac
kazuki.nakayama <kazuki04292003@gmail.com>
2022-03-05 15:33:41 +0900

    Initial Commit

---
+++ QRCodeReaderSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QRCodeReaderSample
+//
+//  Created by 中山一輝 on 2022/03/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QRCodeReaderSample/QRCodeReaderSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeReaderSampleApp.swift
+//  QRCodeReaderSample
+//
+//  Created by 中山一輝 on 2022/03/05.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeReaderSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
